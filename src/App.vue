6679c1cbe17cfe8db14ada659b22e8c886310cc3
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-30 15:51:28 +0300

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
   },
 };
 </script>
